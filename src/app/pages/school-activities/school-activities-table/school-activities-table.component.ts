136df119c268a28979aa7df385ad343ad99aad66
Gerry Anderson <frostggwp706@gmail.com>
2024-12-11 15:44:55 +0700

    #Table

--- src/app/pages/school-activities/school-activities-table/school-activities-table.component.ts
+++ src/app/pages/school-activities/school-activities-table/school-activities-table.component.ts
@@ -1,16 +1,161 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {faDeleteLeft, faEdit, faSave, faTrash, faUpload} from "@fortawesome/free-solid-svg-icons";
 
+
+
+interface SchoolActivity {
+   school_activity_name: string;
+    category_total: number;
+    school_activity_id: number;
+    symbol: string;
+    description: string;
+}
 @Component({
     selector: 'school-activities-table',
     styleUrls: ['school-activities-table.component.scss'],
     templateUrl: 'school-activities-table.component.html',
 })
 export class SchoolActivitiesTableComponent implements OnInit{
-    public table: any;
+    searchText: string ='';
+    faEdit = faEdit;
+    faDelete = faTrash;
+    faUpdate = faSave;
+    currentPage = 1;
+    category: any;
+    dummyData: any;
+    constructor(){
 
-    constructor() {}
+    }
+    ngOnInit(): void {
+        this.setPage();
+    }
+    filter(event: any) {
 
-    ngOnInit(): void {}
+    }
 
+    setPage($event: any = null) {
+        this.dummyData =  [
+            {
+                "school_activity_id": 1,
+                "category_total": 35,
+                "school_activity_name": "Extracurricular",
+                "description": "Activities conducted outside the regular academic curriculum, like sports and arts.",
+                "category_list": [
+                    {
+                        'category_id': 1,
+                        'category_name': 'tes1',
+                        'description': '',
+                        'status': 'Active',
+
+                    },
+                    {
+                        'category_id': 21,
+                        'category_name': 'tes12',
+                        'description': '',
+                        'status': 'Active',
+                    },
+                    {
+                        'category_id': 2,
+                        'category_name': 'tes4',
+                        'description': '',
+                        'status': 'Active',
+                    }
+                ]
+            },
+            {
+                "school_activity_id": 2,
+                "category_total": 12,
+                "school_activity_name": "Intracurricular",
+                "description": "Activities integrated into the academic curriculum, like laboratory sessions and workshops.",
+                "category_list": [
+                    {
+                        'category_id': 1,
+                        'category_name': 'tes1',
+                        'description': '',
+                        'status': 'Active',
+                    },
+                    {
+                        'category_id': 21,
+                        'category_name': 'tes112121212',
+                        'description': '',
+                        'status': 'Active',
+                    },
+                    {
+                        'category_id': 2,
+                        'category_name': 'tes4222',
+                        'description': '',
+                        'status': 'Active',
+                    }
+                ]
+            },
+            {
+                "school_activity_id": 3,
+                "category_total": 9,
+                "school_activity_name": "Lomba",
+                "description": "Competitions organized to encourage participation in various skills and fields.",
+                "category_list": [
+                    {
+                        'category_id': 1,
+                        'category_name': 'tasasasa1',
+                        'description': '',
+                        'status': 'Active',
+                    },
+                    {
+                        'category_id': 21,
+                        'category_name': 't1212122',
+                        'description': '',
+
+                    },
+                    {
+                        'category_id': 2,
+                        'category_name': 'tes4',
+                        'description': '',
+                    }
+                ]
+            },
+            {
+                "school_activity_id": 4,
+                "category_total": 3,
+                "school_activity_name": "Olimpiade",
+                "description": "Olympiads or academic competitions focused on specific subjects like math and science.",
+                "category_list": [
+                    {
+                        'category_id': 1,
+                        'category_name': 'tes1',
+                        'description': '',
+
+                    },
+                    {
+                        'category_id': 21,
+                        'category_name': 'tes12',
+                        'description': '',
+                    },
+                    {
+                        'category_id': 2,
+                        'category_name': 'tes4',
+                        'description': '',
+                    }
+                ]
+            }
+        ]
+    }
+    editingRow: any = null;
+    editingColumn: any = null;
+    isEditing(row: any, column: any): boolean {
+        return this.editingRow === row && this.editingColumn == column;
+    }
+
+    startEdit(row: any, column: any): void {
+        this.editingRow = row;
+        this.editingColumn = column;
+    }
+
+
+    onEditComplete(row: any): void {
+        this.editingRow = null;
+        this.editingColumn = null;
+        console.log('Updated category_name:', row.category_name);
+
+    }
 }
 
--- src/app/pages/school-activities/school-activities-table/school-activities-table.module.ts
+++ src/app/pages/school-activities/school-activities-table/school-activities-table.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SchoolActivitiesTableComponent} from "./school-activities-table.component";
-import {PipeModule} from "../../../../shared/pipes/pipe.module";
-import {MatCard} from "@angular/material/card";
-import {MatIcon} from "@angular/material/icon";
-import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
-import {MatTable} from "@angular/material/table";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatInputModule} from "@angular/material/input";
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SchoolActivitiesTableComponent } from "./school-activities-table.component";
+import { PipeModule } from "../../../../shared/pipes/pipe.module";
+import { MatCard } from "@angular/material/card";
+import { MatIcon } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTableModule } from "@angular/material/table";  // This is the correct import
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatInputModule } from "@angular/material/input";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { NgxPaginationModule } from "ngx-pagination";
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 
 @NgModule({
     imports: [
@@ -24,17 +25,18 @@ import {MatInputModule} from "@angular/material/input";
         PipeModule,
         MatCard,
         MatIcon,
-        MatFormField,
+        MatFormFieldModule,
         ReactiveFormsModule,
-        MatTable,
+        MatTableModule,
         MatProgressSpinner,
         MatPaginator,
         MatInputModule,
-        MatFormFieldModule
+        FaIconComponent,
+        NgxPaginationModule,
+        NgxDatatableModule
     ],
     declarations: [SchoolActivitiesTableComponent],
     exports: [SchoolActivitiesTableComponent],
     providers: []
 })
-export class SchoolActivitiesTableModule {
-}
+export class SchoolActivitiesTableModule { }
